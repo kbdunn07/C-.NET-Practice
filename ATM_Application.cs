9c928eb999291e17b71879a9fa727b1e0d3ac035
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-08-27 10:10:28 -0500

    Add files via upload

---
+++ ATM_Application.cs
@@ -0,0 +1,65 @@
+﻿internal class ATM_Application
+{
+    static decimal accBalance = 1000;
+
+    static void Main(string[] args)
+    {
+        int option = 0;
+
+        while (option != 4)
+        {
+            Console.WriteLine("Welcome to ATM Service!\n\n1. Check Balance\n\n2. Withdraw Cash\n\n3. Deposit Cash\n\n4. Quit");
+
+            option = Convert.ToInt32(Console.ReadLine());
+
+            if (option == 1)
+            {
+                Console.WriteLine("Your Current Balance is $" + accBalance);
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine("Enter Amount to Withdraw:");
+
+                string tempamount = Console.ReadLine();
+                decimal amount = Convert.ToDecimal(tempamount);
+
+                if (amount <= accBalance)
+                {
+                    accBalance -= amount;
+                    Console.WriteLine("Withdraw Success.");
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient Funds.");
+                }
+            }
+            else if (option == 3)
+            {
+                Console.WriteLine("Enter Amount to Deposit:");
+
+                string tempamount = Console.ReadLine();
+                decimal amount = Convert.ToDecimal(tempamount);
+
+                if (amount > 0)
+                {
+                    accBalance += amount;
+                    Console.WriteLine("Deposit Successful");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Amount.");
+                }
+            }
+            else if (option == 4)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Invalid Option.");
+            }
+        }
+
+        Console.WriteLine("Thank you for using our ATM Service!");
+    }
+}
---
+++ studentMarksProgram.cs
@@ -0,0 +1,27 @@
+﻿/*
+internal class studentMarkProgram
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many students are in your course?");
+        string tempnum = Console.ReadLine();
+        int studentnum = Convert.ToInt32(tempnum);
+        decimal[] studentMarks = new decimal[studentnum];
+        Console.WriteLine("\nPlease Enter each Student Mark:");
+        for (int i = 0; i < studentMarks.Length; i++)
+        {
+            string tempmark = Console.ReadLine();
+            studentMarks[i] = Convert.ToDecimal(tempmark);
+        }
+
+        Console.WriteLine("\nThere are {0} students within this course.", studentnum);
+        Console.WriteLine("Each of their Marks are as follows:");
+        foreach (decimal m in studentMarks)
+        {
+            Console.WriteLine(m + "%");
+        }
+        Console.WriteLine("\nThe Highest Mark in the course is {0}%, while the lowest is {1}%.", studentMarks.Max(), studentMarks.Min());
+
+    }
+}
+*/
