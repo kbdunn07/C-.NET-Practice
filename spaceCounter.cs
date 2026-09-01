404eb62d7c663e106e5b61fac2c719687c91b20b
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-09-01 09:54:00 -0500

    Add files via upload

---
+++ spaceCounter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+
+    static void spaceCounter(string str)
+    {
+        int count = 0;
+        foreach (char c in str)
+        {
+            if (c == ' ')
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("This string contains {0} spaces!", count);
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a string:");
+        string str = Console.ReadLine();
+        spaceCounter(str);
+    }
+}
