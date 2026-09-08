d24fc08c8cb87d3c8b84bc32c07fbe6cfea9afae
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-09-08 17:09:39 -0500

    Update Classwork-01.cs

--- Classwork-01.cs
+++ Classwork-01.cs
@@ -1,92 +1,148 @@
 using System;
 using System.Linq;
 
-
-public class StudentGradeCalculator
+class StudentGradeCalculator
 {
-
-    static void Part1() 
-        {
-            Console.WriteLine ("Enter your First Name:");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter your Last Name:");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter your Age:");
-            string age = Console.ReadLine();
-            Console.WriteLine("Enter your Major:");
-            string major = Console.ReadLine();
-
-            Console.WriteLine($"\nStudent Information\n----------\nName: {firstName} {lastName}\nAge: {age}\nMajor: {major}");
+    
+    static double CalculateAverage(double[] grades) {
+        double total = 0;
+        foreach (double g in grades) {
+            total+=g;
         }
+        return (total / 5.0);
+    }
 
-    static void Part2and3and4()
-        {
-            int[] grades = new int[5];
-            Console.WriteLine("Enter Grade 1:");
-            int grade = Convert.ToInt32(Console.ReadLine());
-            grades[0] = grade;
-            Console.WriteLine("Enter Grade 2:");
-            grade = Convert.ToInt32(Console.ReadLine());
-            grades[1] = grade;
-            Console.WriteLine("Enter Grade 3:");
-            grade = Convert.ToInt32(Console.ReadLine());
-            grades[2] = grade;
-            Console.WriteLine("Enter Grade 4:");
-            grade = Convert.ToInt32(Console.ReadLine());
-            grades[3] = grade;
-            Console.WriteLine("Enter Grade 5:");
-            grade = Convert.ToInt32(Console.ReadLine());
-            grades[4] = grade;
+    static double FindHighest(double[] grades) {
+        return grades.Max();
+    }
 
-            int total =0;
-            foreach (int g in grades) 
-            {
-                total+=g;
-            }
-
-            double avg = total / 5.0;
-            Console.WriteLine($"Grade Summary\n--------\nTotal: {total}\nAverage: {avg}\nHighest Grade: {grades.Max()}\nLowest Grade: {grades.Min()}");
-            string letter;
+    static double FindLowest(double[] grades) {
+        return grades.Min();
+    }
+    
+    static char CalculateLetterGrade(double avg) {
+        char letter;
             if (avg >= 90)
             {
-                letter = "A";
+                letter = 'A';
             }
             else if (avg < 90 && avg >= 80)
             {
-                letter = "B";
+                letter = 'B';
             }
             else if (avg < 80 && avg >= 70)
             {
-                letter = "C";
+                letter = 'C';
             }
             else if (avg < 70 && avg >= 60)
             {
-                letter = "D";
+                letter = 'D';
             }
             else
             {
-                letter = "F";
+                letter = 'F';
             }
-            string status;
-            if (avg >= 60)
-            {
-                status = "Passed";
-            } else {
-                status = "Failed";                
-            }
-            Console.WriteLine($"Letter Grade: {letter}");
-            Console.WriteLine($"Status: {status}");
-
-        }
-
-
+        return letter;
+    }
 
     
-    public static void Main(string[] args)
+    public static void Main(String[] args)
     {
-        //Part1();
-        Part2and3and4();
+        Console.WriteLine("Enter your first name:");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Enter your last name:");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Enter your age:");
+        string age = Console.ReadLine();
+        Console.WriteLine("Enter your major:");
+        string major = Console.ReadLine();
+
+        Console.WriteLine("\nStudent Information\n----------");
+        Console.WriteLine($"Name: {firstName} {lastName}\nAge: {age}\nMajor: {major}");
+
+        double[] grades = new double[5];
+
+        Console.WriteLine("\nEnter Grade 1:");
+        double grade = Convert.ToDouble(Console.ReadLine());
+        
+        while (grade < 0 || grade > 100) {
+            Console.WriteLine("Invalid grade: Please enter a value between 0 and 100.");
+            grade = Convert.ToDouble(Console.ReadLine());
+        }
+        
+        grades[0] = grade;
         
         
+        Console.WriteLine("Enter Grade 2:");
+        grade = Convert.ToDouble(Console.ReadLine());
+        
+        while (grade < 0 || grade > 100) {
+            Console.WriteLine("Invalid grade: Please enter a value between 0 and 100.");
+            grade = Convert.ToDouble(Console.ReadLine());
+        }
+        
+        grades[1] = grade;
+        
+        
+        Console.WriteLine("Enter Grade 3:");
+        grade = Convert.ToDouble(Console.ReadLine());
+        
+        while (grade < 0 || grade > 100) {
+            Console.WriteLine("Invalid grade: Please enter a value between 0 and 100.");
+            grade = Convert.ToDouble(Console.ReadLine());
+        }
+        
+        grades[2] = grade;
+        
+        
+        Console.WriteLine("Enter Grade 4:");
+        grade = Convert.ToDouble(Console.ReadLine());
+        
+        while (grade < 0 || grade > 100) {
+            Console.WriteLine("Invalid grade: Please enter a value between 0 and 100.");
+            grade = Convert.ToDouble(Console.ReadLine());
+        }
+        
+        grades[3] = grade;
+        
+        
+        Console.WriteLine("Enter Grade 5:");
+        grade = Convert.ToDouble(Console.ReadLine());
+        
+        while (grade < 0 || grade > 100) {
+            Console.WriteLine("Invalid grade: Please enter a value between 0 and 100.");
+            grade = Convert.ToDouble(Console.ReadLine());
+        }
+        
+        grades[4] = grade;
+
+        double total = 0;
+        foreach (double g in grades) {
+            total+=g;
+        }
+
+        Console.WriteLine("\nGrade Summary\n----------");
+        Console.WriteLine($"Total: {total}\nAverage: {CalculateAverage(grades)}\nHighest Grade: {FindHighest(grades)}\nLowest Grade: {FindLowest(grades)}\n\nFinal Letter Grade: {CalculateLetterGrade(CalculateAverage(grades))}");
+        string status;
+        if (CalculateAverage(grades) >= 60) {
+            status = "Passed";
+        } else {
+            status = "Failed";
+        }
+        Console.WriteLine("Status: " + status);
+        Console.WriteLine("\nThank you for using the Student Grade Calculator!");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
